2efdbbe7a950b9feb50b5fba141e7a95faf181ac
Hazel <hazel@edibly.ca>
2019-10-03 13:34:39 -0600

    Finished ios inapppurchases

--- lib/ui/date_detail/CategoryDetailBody.dart
+++ lib/ui/date_detail/CategoryDetailBody.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cool_date_night/Theme.dart' as Theme;
 import 'package:cool_date_night/bloc_helper/helper.dart';
@@ -6,9 +8,9 @@ import 'package:cool_date_night/ui/date_list/DateList.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart' as prefix0;
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:in_app_purchase/in_app_purchase.dart';
 
 class CategoryDetailBody extends StatefulWidget {
-
   final Map userProfile;
   final Category category;
   CategoryDetailBody(this.userProfile, this.category);
@@ -20,6 +22,31 @@ class CategoryDetailBody extends StatefulWidget {
 class _CategoryDetailBody extends State<CategoryDetailBody> {
   Map userProfile;
   final Category category;
+  StreamSubscription<List<PurchaseDetails>> _subscription;
+
+  @override
+  void initState() {
+    print("Initstate");
+    final Stream purchaseUpdates =
+        InAppPurchaseConnection.instance.purchaseUpdatedStream;
+    _subscription = purchaseUpdates.listen((purchases) {
+        print(purchases[0].productID);
+      if (purchases.length > 0) {
+        print(purchases.length);
+        Firestore.instance
+            .collection('users')
+            .document(userProfile['uid'])
+            .updateData({'isPaid': true});
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _subscription.cancel();
+    super.dispose();
+  }
 
   _CategoryDetailBody(this.userProfile, this.category);
   Widget build(BuildContext context) {
@@ -36,39 +63,40 @@ class _CategoryDetailBody extends State<CategoryDetailBody> {
               child: Column(
                 children: <Widget>[
                   ConstrainedBox(
-                    constraints: prefix0.BoxConstraints(minHeight: prefix0.MediaQuery.of(context).size.height/2),
-                    child: 
-                  Container(
-                      width: MediaQuery.of(context).size.width,
-                      child: Column(
-                        children: <Widget>[
-                          prefix0.SizedBox(height: 30),
-                          ClipOval(
-                              child: Container(
-                                  child: Avatar(
-                            heroTag: category.name,
-                            imagePath: category.image,
-                            radius: 70,
+                      constraints: prefix0.BoxConstraints(
+                          minHeight:
+                              prefix0.MediaQuery.of(context).size.height / 2),
+                      child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          child: Column(
+                            children: <Widget>[
+                              prefix0.SizedBox(height: 30),
+                              ClipOval(
+                                  child: Container(
+                                      child: Avatar(
+                                heroTag: category.name,
+                                imagePath: category.image,
+                                radius: 70,
+                              ))),
+                              SizedBox(height: 20),
+                              AutoSizeText(
+                                category.name,
+                                textScaleFactor: 1,
+                                style: Theme.TextStyles.dateTitle,
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Container(
+                                  margin: EdgeInsets.only(left: 20, right: 20),
+                                  child: Text(
+                                    category.longDescription,
+                                    textAlign: TextAlign.justify,
+                                    style: Theme.TextStyles.bodyLight,
+                                  )),
+                              SizedBox(height: 10),
+                            ],
                           ))),
-                          SizedBox(height: 20),
-                          AutoSizeText(
-                            category.name,
-                            textScaleFactor: 1,
-                            style: Theme.TextStyles.dateTitle,
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Container(
-                              margin: EdgeInsets.only(left: 20, right: 20),
-                              child: Text(
-                                category.longDescription,
-                                textAlign: TextAlign.justify,
-                                style: Theme.TextStyles.bodyLight,
-                              )),
-                          SizedBox(height: 10),
-                        ],
-                      ))),
                   // userProfile.data['date_mate'] == null
                   //     ? Column(
                   //         children: <Widget>[
